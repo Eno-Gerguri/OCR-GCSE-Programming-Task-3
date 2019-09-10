b47e66d2e6a83b6b54d3eaeb60d0718ab94c4d1d
Eno-Gerguri <enogerguri@outlook.com>
2019-09-10 19:05:32 +0100

    Python file updates

--- Version 1 (Multiple Files)/Authenticate_User.py
+++ Version 1 (Multiple Files)/Authenticate_User.py
@@ -1,2 +1,8 @@
-def main_menu():
+def starting_menu():
     option_to_sign_in_or_create_account = input("Sing in\nor\nCreate an account\n\n")
+
+from Database_Manager import init, check_if_user_exists, close
+
+init()
+check_if_user_exists(None)
+close()
